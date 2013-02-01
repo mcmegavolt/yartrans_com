8ec55c86dbd1a96f66293e9c9c5a3f6478d00d2d
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-02-01 16:06:00 +0200

    setup production database

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,16 +27,7 @@ server "91.223.223.135", :web, :app, :db, :primary => true
 
 set :keep_releases, 4
 
-# Fix log/ and pids/ permissions
-after "deploy:setup", "deploy:fix_setup_permissions"
-
-# Fix permissions
-before "deploy:start", "deploy:fix_permissions"
-after "deploy:restart", "deploy:fix_permissions"
-after "assetsrecompile", "deploy:fix_permissions"
-
-
-      after 'deploy:update_code', :roles => :app do
+after 'deploy:update_code', :roles => :app do
   # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
   run "rm -f #{current_release}/config/database.yml"
   run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
@@ -54,19 +45,4 @@ namespace :deploy do
   task :stop do
     run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
-end
-
-task :fix_setup_permissions, :roles => :app, :except => { :no_release => true } do
-  run "#{sudo} chgrp #{user_rails} #{shared_path}/log"
-  run "#{sudo} chgrp #{user_rails} #{shared_path}/pids"
-end
-
-task :fix_permissions, :roles => :app, :except => { :no_release => true } do
-  # To prevent access errors while moving/deleting
-  run "#{sudo} chmod 775 #{current_path}/log"
-  run "#{sudo} find #{current_path}/log/ -type f -exec chmod 664 {} \\;"
-  run "#{sudo} find #{current_path}/log/ -exec chown #{user}:#{user_rails} {} \\;"
-  run "#{sudo} find #{current_path}/tmp/ -type f -exec chmod 664 {} \\;"
-  run "#{sudo} find #{current_path}/tmp/ -type d -exec chmod 775 {} \\;"
-  run "#{sudo} find #{current_path}/tmp/ -exec chown #{user}:#{user_rails} {} \\;"
 end
