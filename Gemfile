5f146b6f64797601c2d92f5fdd3a8940b645d70f
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-02-01 13:29:32 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'jquery-rails'
+gem 'haml'
+gem 'inherited_resources'
+gem 'thin'
+
+gem 'unicorn'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass'
+  gem 'font-awesome-sass-rails'
+  gem 'haml-rails'
+end
+
+group :development do
+  gem 'sqlite3'
+  gem 'capistrano'
+  gem 'rvm-capistrano'
+end
+
+group :production do
+  gem 'mysql2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yartrans::Application.config.secret_token = 'fa725ab5888d56b33273e81de467476a722c2075165e0cd3f39f4a63d6d475089cf4c29adfb6e8e3d570f3b08d06b6a0aec105f832258bb19994d040954ee0f4'
