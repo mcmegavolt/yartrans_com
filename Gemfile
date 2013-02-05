a83a08aeed2093c90c28051ad933d9f1b15af37f
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-02-05 23:57:45 +0200

    home index Hello World!

--- Gemfile
+++ Gemfile
@@ -5,6 +5,15 @@ gem 'jquery-rails'
 gem 'haml'
 gem 'inherited_resources'
 gem 'thin'
+gem 'russian'
+gem 'mail'
+gem 'meta-tags', :require => 'meta_tags'
+gem 'sitemap_generator'
+gem 'devise'
+gem 'devise-async'
+gem 'delayed_job_active_record'
+gem 'client_side_validations'
+gem 'ancestry'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
@@ -19,8 +28,16 @@ group :development do
   gem 'sqlite3'
   gem 'capistrano'
   gem 'rvm-capistrano'
+  gem 'letter_opener'
+  gem 'nifty-generators'
+  gem 'populator'
+  gem 'faker'
 end
 
 group :production do
   gem 'mysql2'
+  gem 'backup'
+  gem 'whenever'
 end
+
+gem 'mocha', :group => :test
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Yartrans::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -10,22 +12,20 @@ Yartrans::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
+  #config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
-  # Generate digests for assets URLs
+  # Generate digests for assets URLs.
   config.assets.digest = true
 
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
-
   # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  # (comment out if your front-end server doesn't support this)
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
+  #config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # Use 'X-Accel-Redirect' for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -33,11 +33,8 @@ Yartrans::Application.configure do
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
-  # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
-
   # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
@@ -61,7 +58,9 @@ Yartrans::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = { :host => 'yartrans.ua' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = { :arguments => "-i" }
+
+
 end
