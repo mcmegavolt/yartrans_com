a87af974168e895039bf9184ccd5b19ad8db4f24
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-02-05 13:48:00 +0200

    deploy check

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,140 @@
+# Automatically precompile assets
 load "deploy/assets"
 
-require 'rvm/capistrano' # Для работы rvm
-require 'bundler/capistrano'# Для работы bundler. При изменении гемов bundler автоматически обновит все гемы на сервере, чтобы они в точности соответствовали гемам разработчика.
+# Execute "bundle install" after deploy, but only when really needed
+require "bundler/capistrano"
 
+# RVM integration
+require "rvm/capistrano"
 
+# Application name
 set :application, "yartrans"
-set :rails_env, "production"
-set :using_rvm, true
-set :rvm_type, :system
+
+# Application environment
+set :rails_env, :production
+
+# Deploy username and sudo username
 set :user, "root"
 set :user_rails, "root"
+
+# App Domain
+set :domain, "91.223.223.135"
+
+# We don't want to use sudo (root) - for security reasons
+set :use_sudo, false
+
+# Target ruby version
+set :rvm_ruby_string, '1.9.3'
+
+# System-wide RVM installation
+set :rvm_type, :system
+
+# We use sudo (root) for system-wide RVM installation
 set :rvm_install_with_sudo, true
 
-set :deploy_to, "/srv/#{application}"
-set :deploy_via, :copy
-set :normalize_asset_timestamps, false
-
-set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
-set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
-
+# git is our SCM
 set :scm, :git
-set :repository, ".git"
+
+# Use github repository
+#set :repository, "git@github.com:mcmegavolt/yartrans.git"
+set :repository,  "/Ubuntu One/rails_projects/#{application}/.git"
+
+# master is our default git branch
 set :branch, "master"
 
-server "91.223.223.135", :web, :app, :db, :primary => true
+# Deploy via github
+#set :deploy_via, :remote_cache
+set :deploy_via, :copy
+set :deploy_to, "/srv/#{application}"
 
-set :keep_releases, 4
+# We have all components of the app on the same server
+server domain, :app, :web, :db, :primary => true
 
-after 'deploy:update_code', :roles => :app do
-  # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
-  run "rm -f #{current_release}/config/database.yml"
-  run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
+# Install RVM and Ruby before deploy
+before "deploy:setup", "rvm:install_rvm"
+before "deploy:setup", "rvm:install_ruby"
+
+# Apply default RVM version for the current account
+after "deploy:setup", "deploy:set_rvm_version"
+
+# Fix log/ and pids/ permissions
+after "deploy:setup", "deploy:fix_setup_permissions"
+
+# Fix permissions
+before "deploy:start", "deploy:fix_permissions"
+after "deploy:restart", "deploy:fix_permissions"
+after "assetsrecompile", "deploy:fix_permissions"
+
+# Clean-up old releases
+after "deploy:restart", "deploy:cleanup"
+
+# Unicorn config
+set :unicorn_config, "#{current_path}/config/unicorn.conf.rb"
+set :unicorn_binary, "bash -c 'source /etc/profile.d/rvm.sh && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D'"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+set :su_rails, "sudo -u #{user_rails}"
+
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do
+    # Start unicorn server using sudo (rails)
+    run "cd #{current_path} && #{su_rails} #{unicorn_binary}"
+  end
+
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill `cat #{unicorn_pid}`; fi"
+  end
+
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill -s QUIT `cat #{unicorn_pid}`; fi"
+  end
+
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill -s USR2 `cat #{unicorn_pid}`; fi"
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end
+
+  task :set_rvm_version, :roles => :app, :except => { :no_release => true } do
+    run "source /etc/profile.d/rvm.sh && rvm use #{rvm_ruby_string} --default"
+  end
+
+  task :fix_setup_permissions, :roles => :app, :except => { :no_release => true } do
+    run "#{sudo} chgrp #{user_rails} #{shared_path}/log"
+    run "#{sudo} chgrp #{user_rails} #{shared_path}/pids"
+  end
+
+  task :fix_permissions, :roles => :app, :except => { :no_release => true } do
+    # To prevent access errors while moving/deleting
+    run "#{sudo} chmod 775 #{current_path}/log"
+    run "#{sudo} find #{current_path}/log/ -type f -exec chmod 664 {} \\;"
+    run "#{sudo} find #{current_path}/log/ -exec chown #{user}:#{user_rails} {} \\;"
+    run "#{sudo} find #{current_path}/tmp/ -type f -exec chmod 664 {} \\;"
+    run "#{sudo} find #{current_path}/tmp/ -type d -exec chmod 775 {} \\;"
+    run "#{sudo} find #{current_path}/tmp/ -exec chown #{user}:#{user_rails} {} \\;"
+  end
+
+  # Precompile assets only when needed
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      # If this is our first deploy - don't check for the previous version
+      if remote_file_exists?(current_path)
+        from = source.next_revision(current_revision)
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assetsrecompile}
+        else
+          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        end
+      else
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assetsrecompile}
+      end
+    end
+  end
 end
 
-# Далее идут правила для перезапуска unicorn. Их стоит просто принять на веру - они работают.
-# В случае с Rails 3 приложениями стоит заменять bundle exec unicorn_rails на bundle exec unicorn
-namespace :deploy do
-  task :restart do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
-  end
-  task :start do
-    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
-  end
-  task :stop do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
-  end
+# Helper function
+def remote_file_exists?(full_path)
+  'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
 end
