e450be9989bb6ac52822e296f8e92546d5a6bd1c
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-02-05 14:03:22 +0200

    deploy check

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,54 +1,23 @@
 # Automatically precompile assets
 load "deploy/assets"
-
-# Execute "bundle install" after deploy, but only when really needed
+require "rvm/capistrano"
 require "bundler/capistrano"
 
-# RVM integration
-require "rvm/capistrano"
-
-# Application name
-set :application, "yartrans"
-
-# Application environment
-set :rails_env, :production
-
-# Deploy username and sudo username
-set :user, "root"
-set :user_rails, "root"
-
-# App Domain
-set :domain, "91.223.223.135"
-
-# We don't want to use sudo (root) - for security reasons
-set :use_sudo, false
-
-# Target ruby version
-set :rvm_ruby_string, '1.9.3'
-
-# System-wide RVM installation
+set :using_rvm, true
 set :rvm_type, :system
 
-# We use sudo (root) for system-wide RVM installation
-set :rvm_install_with_sudo, true
+set :application, "yartrans"
+set :user, "root"
 
-# git is our SCM
-set :scm, :git
-
-# Use github repository
-#set :repository, "git@github.com:mcmegavolt/yartrans.git"
-set :repository,  "/Ubuntu One/rails_projects/#{application}/.git"
-
-# master is our default git branch
-set :branch, "master"
-
-# Deploy via github
-#set :deploy_via, :remote_cache
-set :deploy_via, :copy
 set :deploy_to, "/srv/#{application}"
+set :deploy_via, :copy
+set :normalize_asset_timestamps, false
+set :scm, :git
+set :repository,  ".git"
 
-# We have all components of the app on the same server
-server domain, :app, :web, :db, :primary => true
+server "91.223.223.135", :web, :app, :db, :primary => true
+
+set :keep_releases, 4
 
 # Install RVM and Ruby before deploy
 before "deploy:setup", "rvm:install_rvm"
@@ -72,24 +41,24 @@ after "deploy:restart", "deploy:cleanup"
 set :unicorn_config, "#{current_path}/config/unicorn.conf.rb"
 set :unicorn_binary, "bash -c 'source /etc/profile.d/rvm.sh && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D'"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
-set :su_rails, "sudo -u #{user_rails}"
+set :su_rails, "sudo -u #{user}"
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
     # Start unicorn server using sudo (rails)
-    run "cd #{current_path} && #{su_rails} #{unicorn_binary}"
+    run "cd #{current_path} && #{user} #{unicorn_binary}"
   end
 
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill `cat #{unicorn_pid}`; fi"
+    run "if [ -f #{unicorn_pid} ]; then #{user} kill `cat #{unicorn_pid}`; fi"
   end
 
   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill -s QUIT `cat #{unicorn_pid}`; fi"
+    run "if [ -f #{unicorn_pid} ]; then #{user} kill -s QUIT `cat #{unicorn_pid}`; fi"
   end
 
   task :reload, :roles => :app, :except => { :no_release => true } do
-    run "if [ -f #{unicorn_pid} ]; then #{su_rails} kill -s USR2 `cat #{unicorn_pid}`; fi"
+    run "if [ -f #{unicorn_pid} ]; then #{user} kill -s USR2 `cat #{unicorn_pid}`; fi"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
@@ -102,18 +71,18 @@ namespace :deploy do
   end
 
   task :fix_setup_permissions, :roles => :app, :except => { :no_release => true } do
-    run "#{sudo} chgrp #{user_rails} #{shared_path}/log"
-    run "#{sudo} chgrp #{user_rails} #{shared_path}/pids"
+    run "#{sudo} chgrp #{user} #{shared_path}/log"
+    run "#{sudo} chgrp #{user} #{shared_path}/pids"
   end
 
   task :fix_permissions, :roles => :app, :except => { :no_release => true } do
     # To prevent access errors while moving/deleting
     run "#{sudo} chmod 775 #{current_path}/log"
     run "#{sudo} find #{current_path}/log/ -type f -exec chmod 664 {} \\;"
-    run "#{sudo} find #{current_path}/log/ -exec chown #{user}:#{user_rails} {} \\;"
+    run "#{sudo} find #{current_path}/log/ -exec chown #{user}:#{user} {} \\;"
     run "#{sudo} find #{current_path}/tmp/ -type f -exec chmod 664 {} \\;"
     run "#{sudo} find #{current_path}/tmp/ -type d -exec chmod 775 {} \\;"
-    run "#{sudo} find #{current_path}/tmp/ -exec chown #{user}:#{user_rails} {} \\;"
+    run "#{sudo} find #{current_path}/tmp/ -exec chown #{user}:#{user} {} \\;"
   end
 
   # Precompile assets only when needed
