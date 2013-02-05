e6577bbb051e9b6cc319037578a5aa5ede49a3b6
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-02-05 15:39:58 +0200

    deploy check

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ after "deploy:restart", "deploy:cleanup"
 
 # Unicorn config
 set :unicorn_config, "#{current_path}/config/unicorn.conf.rb"
-set :unicorn_binary, "bash -c 'source /etc/profile.d/rvm.sh && bundle exec unicorn -c #{unicorn_config} -E #{rails_env} -D'"
+set :unicorn_binary, "bash -c 'source /etc/profile.d/rvm.sh && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D'"
 set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
 set :su_rails, "sudo -u #{user}"
 
