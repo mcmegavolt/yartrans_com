2a6d5a3fa67fa666a42f52cddf8d0bc747a204c7
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-03-25 14:26:37 +0200

    modified:   db/migrate/20130323112501_remove_unnecessary_columns_from_release_app.rb

--- db/migrate/20130323112501_remove_unnecessary_columns_from_release_app.rb
+++ db/migrate/20130323112501_remove_unnecessary_columns_from_release_app.rb
@@ -1,6 +1,6 @@
 class RemoveUnnecessaryColumnsFromReleaseApp < ActiveRecord::Migration
   def change
-  	remove_column :release_apps, :cargo_name, :consignment, :code_number, :unit_id, :unic_count, :in_box_count, :box_count
+  	remove_column :release_apps, :cargo_name, :consignment, :code_number, :unit_id, :unit_count, :in_box_count, :box_count
   	rename_column :release_apps, :additional_info, :notes
   end
 end
