31e1c21e6a0466f187f362bf24d5569a5faee400
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-03-25 14:17:07 +0200

    modified:   db/migrate/20130323111628_remove_unnecessary_columns_from_admission_app.rb 	modified:   db/migrate/20130323113951_add_needed_columns_to_admission_app.rb

--- db/migrate/20130323113951_add_needed_columns_to_admission_app.rb
+++ db/migrate/20130323113951_add_needed_columns_to_admission_app.rb
@@ -1,7 +1,7 @@
 class AddNeededColumnsToAdmissionApp < ActiveRecord::Migration
   def change
 
-    remove_column :admission_apps, :additional_info
+    # remove_column :admission_apps, :additional_info
     remove_column :admission_apps, :expected_date
     
     add_column :admission_apps, :admission_time, :time
