38cf8a728968ebe721711869385335e784f40666
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-04-01 14:04:12 +0300

    debuging deploy problem

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,6 +1,6 @@
 
-# # Admission apps
-# Setting.save_default('admission_apps.manager_email', "mcmegavolt@gmail.com")
+# Admission apps
+Setting.save_default('admission_apps.manager_email', "mcmegavolt@gmail.com")
 
-# # Release apps
-# Setting.save_default('release_apps.manager_email', "mcmegavolt@gmail.com")
+# Release apps
+Setting.save_default('release_apps.manager_email', "mcmegavolt@gmail.com")
