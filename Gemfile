6b4194e971bfdf3aed34777deee06ad5ab802e9e
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-04-01 12:20:20 +0300

    debuging deploy problem

--- Gemfile
+++ Gemfile
@@ -32,10 +32,8 @@ gem 'simple_form'
 gem 'cocoon'
 gem 'axlsx_rails'
 
-gem 'rake', '10.0.3'
-
 group :assets do
-  gem 'sass-rails',   '3.2.5'
+  gem 'sass-rails',   '~> 3.2.6'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
   gem 'haml-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,11 +24,11 @@
 
 
   $(function() {
-    $('.datetimepicker').datetimepicker({
+    $(".datetimepicker").datetimepicker({
       pickTime: true,
       pickDate: true,
       pickSeconds: true,
-      language: 'ru-RU'
+      language: "ru-RU"
     });
   });
 
@@ -37,33 +37,33 @@ $(document).ready(function() {
 
   // example showing manipulating the inserted/removed item
 
-  $('#admission_items')
-    .bind('cocoon:before-insert', function(e,task_to_be_added) {
-      task_to_be_added.fadeIn('fast');
+  $("#admission_items")
+    .bind("cocoon:before-insert", function(e,task_to_be_added) {
+      task_to_be_added.fadeIn("fast");
     })
-    // .bind('cocoon:after-insert', function(e, added_task) {
+    // .bind("cocoon:after-insert", function(e, added_task) {
       // e.g. set the background of inserted task
       // added_task.css("background","silver");
     // })
-    .bind('cocoon:before-remove', function(e, task) {
+    .bind("cocoon:before-remove", function(e, task) {
       // allow some time for the animation to complete
-      $(this).data('remove-timeout', 300);
-      // task.fadeOut('fast');
+      $(this).data("remove-timeout", 300);
+      // task.fadeOut("fast");
       task.animate({ height: "0px", opacity: 0 }, 300 );
     });
 
-  $('#release_items')
-    .bind('cocoon:before-insert', function(e,task_to_be_added) {
-      task_to_be_added.fadeIn('fast');
+  $("#release_items")
+    .bind("cocoon:before-insert", function(e,task_to_be_added) {
+      task_to_be_added.fadeIn("fast");
     })
-    // .bind('cocoon:after-insert', function(e, added_task) {
+    // .bind("cocoon:after-insert", function(e, added_task) {
       // e.g. set the background of inserted task
       // added_task.css("background","silver");
     // })
-    .bind('cocoon:before-remove', function(e, task) {
+    .bind("cocoon:before-remove", function(e, task) {
       // allow some time for the animation to complete
-      $(this).data('remove-timeout', 300);
-      // task.fadeOut('fast');
+      $(this).data("remove-timeout", 300);
+      // task.fadeOut("fast");
       task.animate({ height: "0px", opacity: 0 }, 300 );
     });
     
