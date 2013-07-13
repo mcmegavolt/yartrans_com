579fd3b6fea23916a553e0f06d773a2f5945b814
Aleksansr Ponomarenko <mcmegavolt@gmail.com>
2013-07-13 22:22:48 +0300

    whenever job_template update

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, "/home/yartrans/yartrans/shared/log/cron_log.log"
-set :job_template, "bash -l -c ':job'"
+# set :job_template, "bash -l -c ':job'"
 # set :job_template, nil
 # every 1.day, :at => '10:00 am' do
 #   runner "User.debt_warning"
