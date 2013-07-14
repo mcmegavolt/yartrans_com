a1ef215054292e0c2fd846e53399702e1274bf1f
Aleksansr Ponomarenko <mcmegavolt@gmail.com>
2013-07-15 02:18:17 +0300

    status warnings in cabinet

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,6 @@
 set :output, "/home/yartrans/yartrans/shared/log/cron_log.log"
-# set :job_template, "bash -l -c ':job'"
-# set :job_template, nil
-# every 1.day, :at => '10:00 am' do
-#   runner "User.debt_warning"
-# end
 
-every 10.minutes do
+every 1.day, :at => '10:00 am' do
   rake "user:warn"
 end
 
