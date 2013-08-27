b0bb2e3aa27ba1a7313bea83c3e789ca661872fc
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-08-28 00:36:52 +0300

    cosmetic update

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -4,3 +4,6 @@ Setting.save_default('admission_apps.manager_email', "mcmegavolt@gmail.com")
 
 # Release apps
 Setting.save_default('release_apps.manager_email', "mcmegavolt@gmail.com")
+
+# Contact phones
+Setting.save_default('contacts.phones', "(067) 236-11-69, (044) 495-50-10")
