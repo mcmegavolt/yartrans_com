3489f229f824fa0905c1fa87021e8e73c068cacd
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-11-06 21:18:33 +0200

    favicon + footer email setting

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -7,3 +7,6 @@ Setting.save_default('release_apps.manager_email', "mcmegavolt@gmail.com")
 
 # Contact phones
 Setting.save_default('contacts.phones', "(067) 236-11-69, (044) 495-50-10")
+
+# Contact
+Setting.save_default('contacts.email', "office@yartrans.ua")
