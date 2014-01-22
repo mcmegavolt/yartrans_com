1b1992ebe5ee1d502d0a6f5d08472bff6d65874b
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2014-01-23 00:49:51 +0200

    feedback mail_to fix try

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,11 +1,10 @@
 class FeedbackMailer < ActionMailer::Base
 
 	default :from => "\"YarTrans Logistic, site service\" <service@yartrans.ua>"
-  default to: SiteSettings["feedback.email"]
 
   def feedback(feedback)
     @feedback = feedback
-    mail(:subject => 'Форма обратного звонка')
+    mail(:to => SiteSettings["feedback.email"], :subject => 'Форма обратного звонка')
   end
 
 end
